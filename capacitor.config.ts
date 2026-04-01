6df081268ad2d4cd5b98205ec080be4f9884aefd
Ivan Garcia <fgarcivan@gmail.com>
2026-04-01 04:26:30 +0200

    feat: bypass native splash screen for seamless cinematic transition

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.cercedense.app2',
   appName: 'Club Atletico Cercedense',
-  webDir: 'out'
+  webDir: 'out',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      backgroundColor: "#000000"
+    }
+  }
 };
 
 export default config;
